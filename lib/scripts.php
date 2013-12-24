f8c3fa947f0959be4bd845ecf7fdfd3602692f07
Tyrantelf <tyrantelf@iongaming.org>
2013-12-24 13:09:19 -0600

    images, footer work, etc.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '341630981e2b0764351c55ae28e6d901');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a0727c3ec858994eb45e966c771051b0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
