eb9cf5f7d4a1db0d274152780d1167193b3aef9f
Tyrantelf <tyrantelf@iongaming.org>
2013-12-24 20:13:21 -0600

    Finished up, YES :D

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a0727c3ec858994eb45e966c771051b0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a2a66ce9de8fb6300c65db399064cafe');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/footer.php
+++ templates/footer.php
@@ -1,39 +1,44 @@
 <div class="bluewrap">
-  <footer class="content-info container" role="contentinfo">
-	<div class="row section-header">
-	  <div class="col-md-12">
-	  	<h1 class="med-font">Get in Touch</h1>
-	  </div>
-	</div>
-	<div class="row">
-		<div class="col-md-1"></div>
-		<div class="col-md-10">
-			<div class="col-md-4">
-			  <div class="cont-teamspeak"></div>
-			  <div class="feat-headers">Teamspeak</div>
-			  <div class="feat-desc">teamspeak.feed-the-beast.com<br />Password: mcepoch1<br />Channel: Universal Electricity</div>
-			</div>
-			<div class="col-md-4">
-			  <div class="cont-irc"></div>
-			  <div class="feat-headers">IRC</div>
-			  <div class="feat-desc">Come talk to the developers.<br />#universalelectricity on<br />irc.esper.net:5555</div>
-			</div>
-			<div class="col-md-4">
-			  <div class="cont-forums"></div>
-			  <div class="feat-headers">Forums</div>
-			  <div class="feat-desc">Discuss the latest developments<br />in Universal Electricity.<br />universalelectricty.com/forums</div>
+	<footer class="content-info container" role="contentinfo">
+		<div class="row section-header">
+			<div class="col-md-12">
+				<h1 class="med-font">Get in Touch</h1>
 			</div>
 		</div>
-		<div class="col-md-1"></div>
-	</div>
-	<div class="spacer"></div>
-    <div class="row">
-      <div class="col-lg-12">
-        <?php dynamic_sidebar('sidebar-footer'); ?>
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-      </div>
-    </div>
-  </footer>
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-10">
+				<div class="col-md-4">
+					<div class="cont-teamspeak"></div>
+					<div class="feat-headers">Teamspeak</div>
+					<div class="feat-desc">teamspeak.feed-the-beast.com<br />Password: mcepoch1<br />Channel: Universal Electricity</div>
+				</div>
+				<div class="col-md-4">
+					<div class="cont-irc"></div>
+					<div class="feat-headers">IRC</div>
+					<div class="feat-desc">Come talk to the developers.<br />#universalelectricity on<br />irc.esper.net:5555</div>
+				</div>
+				<div class="col-md-4">
+					<div class="cont-forums"></div>
+					<div class="feat-headers">Forums</div>
+					<div class="feat-desc">Discuss the latest developments<br />in Universal Electricity.<br />universalelectricty.com/forums</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+		<div class="spacer"></div>
+		<div class="row">
+			<div class="col-lg-12 section-header">
+				<?php dynamic_sidebar('sidebar-footer'); ?>
+				<p>Content &copy; 2010-<?php echo date('Y'); ?> Calclavia - About - Copyright - Terms of Use - Login<br />
+				Site Design &copy; <?php echo date('Y'); ?> Tyrantelf<br />
+				Proudly Powered by Wordpress</p>
+				<div class="spacer"></div>
+				<div class="no-underline"><a class="no-underline" href="https://www.creeperhost.net/"><img class="sponsor" src="<?php echo get_template_directory_uri(); ?>/assets/img/creeper-host.png" /></a></div>
+				<div class="spacer"></div>
+			</div>
+		</div>
+	</footer>
 </div>
 
 <?php wp_footer(); ?>
