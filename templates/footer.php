e70f0bedc157991a242d87c6317d87ba567a7111
Tyrantelf <tyrantelf@iongaming.org>
2013-12-26 14:14:37 -0600

    Update theme info, modify footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,31 +1,5 @@
-<div class="bluewrap">
+<div class="footerwrap bluewrap">
 	<footer class="content-info container" role="contentinfo">
-		<div class="row section-header">
-			<div class="col-md-12">
-				<h1 class="med-font">Get in Touch</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-1"></div>
-			<div class="col-md-10">
-				<div class="col-md-4">
-					<div class="cont-teamspeak"></div>
-					<div class="feat-headers">Teamspeak</div>
-					<div class="feat-desc">teamspeak.feed-the-beast.com<br />Password: mcepoch1<br />Channel: Universal Electricity</div>
-				</div>
-				<div class="col-md-4">
-					<div class="cont-irc"></div>
-					<div class="feat-headers">IRC</div>
-					<div class="feat-desc">Come talk to the developers.<br />#universalelectricity on<br />irc.esper.net:5555</div>
-				</div>
-				<div class="col-md-4">
-					<div class="cont-forums"></div>
-					<div class="feat-headers">Forums</div>
-					<div class="feat-desc">Discuss the latest developments<br />in Universal Electricity.<br />universalelectricty.com/forums</div>
-				</div>
-			</div>
-			<div class="col-md-1"></div>
-		</div>
 		<div class="spacer"></div>
 		<div class="row">
 			<div class="col-lg-12 section-header">
