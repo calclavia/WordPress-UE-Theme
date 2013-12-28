832cb758cdba005265b456763579b2b9ed6fbaed
Calclavia <admin@calclavia.com>
2013-12-28 19:21:14 +0800

    Separated some CSS into primary.css for easy editing

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,6 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b515bbb59915535cf772f5bf18d785a');
+  wp_enqueue_style('ue_main', get_template_directory_uri() . '/assets/css/primary.css');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
