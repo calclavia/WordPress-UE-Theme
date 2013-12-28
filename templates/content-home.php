3de99ec91d8a3df8fdcaa28dc7b9e76ccb6d321d
Calclavia <admin@calclavia.com>
2013-12-28 18:16:45 +0800

    Made some things more dynamic/editable

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,5 +1,19 @@
+<div class="bluewrap ue-header">
+	<div class="container" style="text-align: center;">
+        <div class="row">
+			<h1>
+                <span class="med-font" style="font-size: 72px;"><?php bloginfo("name") ?></span>
+                <br />
+                <?php bloginfo("description") ?>
+            </h1>
+            <a class="btn btn-default btn-lg" href="download"><i class="glyphicon glyphicon-download-alt"></i>&nbsp;&nbsp;Download</a>
+            <div class="spacer"></div>
+        </div>
+    </div>
+</div>
+
 <?php while (have_posts()) : the_post(); ?>
-  <?php remove_filter( 'the_content', 'wpautop' ); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+    <?php remove_filter( 'the_content', 'wpautop' ); ?>
+    <?php the_content(); ?>
+    <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
--- templates/footer.php
+++ templates/footer.php
@@ -4,11 +4,6 @@
 		<div class="row">
 			<div class="col-lg-12 section-header">
 				<?php dynamic_sidebar('sidebar-footer'); ?>
-				<p>Content &copy; 2010-<?php echo date('Y'); ?> Calclavia - About - Copyright - Terms of Use - Login<br />
-				Site Design &copy; <?php echo date('Y'); ?> Tyrantelf<br />
-				Proudly Powered by Wordpress</p>
-				<div class="spacer"></div>
-				<div class="no-underline"><a class="no-underline" href="https://www.creeperhost.net/"><img class="sponsor" src="<?php echo get_template_directory_uri(); ?>/assets/img/creeper-host.png" /></a></div>
 				<div class="spacer"></div>
 			</div>
 		</div>
