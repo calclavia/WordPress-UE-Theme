80e846ad004237bf8fe068017d3b2a1bfece0ef0
Calclavia <admin@calclavia.com>
2013-12-28 20:15:34 +0800

    Attempt to fix page name

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,5 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
-    <h1><?php wp_title(); ?></h1>
+    <h1><?php echo $pagename; ?></h1>
     <?php the_content(); ?>
     <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
