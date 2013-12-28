71725368978277da84c68c57b6a0ea26b48babe9
Calclavia <admin@calclavia.com>
2013-12-28 19:54:47 +0800

    Added ability to customize theme primary color

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,37 @@
 /**
  * Custom functions
  */
+function ue_customize_register($wp_customize)
+{
+    //All our sections, settings, and controls will be added here    
+    $wp_customize->add_setting('primary_color' , array(
+        'default'     => '#0892d0',
+        'transport'   => 'refresh',
+    ));
+    
+    $wp_customize->add_control(new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
+        'label'        => __( 'Primary Color', 'mytheme' ),
+        'section'    => 'colors',
+        'settings'   => 'primary_color',
+    )));
+}
+
+add_action('customize_register', 'ue_customize_register');
+
+function ue_primary_color()
+{
+    ?>
+         <style type="text/css">
+             .bluewrap
+             {
+                background-color: <?php echo get_theme_mod('primary_color'); ?>;
+             }
+             .navbar-default
+             {
+                 background-color: <?php echo get_theme_mod('primary_color'); ?>;
+             }
+         </style>
+    <?php
+}
+
+add_action('wp_head', 'ue_primary_color');
