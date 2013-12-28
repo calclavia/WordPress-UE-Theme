54af9661035ec61a00037ea4ca3ee1284db32553
Calclavia <admin@calclavia.com>
2013-12-28 20:13:32 +0800

    Added wp_title() to pages

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
+    <h1><?php wp_title(); ?></h1>
     <?php the_content(); ?>
     <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
